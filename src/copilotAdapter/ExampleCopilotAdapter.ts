04d350b035a350250ce1aea06a3897c22bda8bd6
Vertika Pandey <vepandey@microsoft.com>
2024-03-11 16:00:05 +0530

    Added copilot adpater interface and implementation

---
+++ src/copilotAdapter/ExampleCopilotAdapter.ts
@@ -0,0 +1,50 @@
+import { ICopilotAdapter, OnPageNavigationCallbackFunction, GetDataRequest, GetDataResponse, ProcessDataRequest, ProcessDataResponse, newEventCallbackFunction } from "./ICopilotAdpter";
+
+declare global {
+    interface SalesForce {
+        opencti: any;
+    }
+    interface Window {
+        sforce: SalesForce;
+        CCaaS: any;
+    }
+}
+
+class ExampleCopilotAdapter implements ICopilotAdapter {
+
+    /**
+     * Function to initialize the scripts and do the operations once it is loaded
+     * 
+     * @returns Promise void
+     */
+    initialize(): Promise<boolean> {
+        // load opencti library
+
+        return Promise.resolve(true);
+    }
+
+    onPageNavigation(callback: OnPageNavigationCallbackFunction): void {
+       // Listen to the page navigation of third party CRM 
+       // and call the callback passed to perform necessary action on page navigation
+    }
+
+    getData(request: GetDataRequest): Promise<GetDataResponse> {
+        // Retrieve the third party knowledge articles for copilot
+    }
+
+    processData(request: ProcessDataRequest): Promise<ProcessDataResponse> {      
+        // need to add appropriate description for this method
+    }
+
+    onNewEvent(callback: newEventCallbackFunction): void {
+        // need to add appropriate description for this method
+    }
+
+}
+
+window.CCaaS = window.CCaaS || {};
+if (!window.CCaaS.CopilotAdapter) {
+    window.CCaaS.CopilotAdapter = ExampleCopilotAdapter;
+}
+
+export default ExampleCopilotAdapter;
---
+++ src/copilotAdapter/ICopilotAdpter.d.ts
@@ -0,0 +1,57 @@
+export enum ObjectType {
+	Case,
+	Email,
+	Chat,
+	PhoneCall
+}
+export enum Priority {
+	High = 1,
+	Meduim = 2,
+	Low = 3
+}
+export enum CaseType {
+	Question = 1,
+	Problem = 2,
+	Request = 3
+}
+interface ThirdPartyObject {
+	objectType: ObjectType;
+	objectId: string; // Record is in Dynamics. Unique identifier not PII or EUII
+}
+
+export interface GetDataRequest extends ThirdPartyObject {}
+interface EmailData {
+	to: string;
+	from: string;
+	subject: string;
+	description: string;
+}
+interface CaseData {
+	title: string;
+	description: string;
+	product_name?: string;
+	priority: Priority;
+	subject?: string;
+	customerName: string;
+	caseType: CaseType;
+}
+export interface GetDataResponse extends ThirdPartyObject {
+	data: EmailData | CaseData | any;
+}
+export interface ProcessDataRequest extends ThirdPartyObject {
+	data: string; //Eg: Draft gerenated by Copilot
+}
+export interface ProcessDataResponse extends ThirdPartyObject {
+	success: boolean;
+	message?: string;
+}
+type OnPageNavigationCallbackFunction = (payload: ThirdPartyObject) => void;
+type newEventCallbackFunction = (payload: any) => void;
+
+export interface ICopilotAdapter {
+	initialize(): Promise<boolean>;
+	onPageNavigation(callback: OnPageNavigationCallbackFunction): void;
+	getData(request: GetDataRequest): Promise<GetDataResponse>;
+	processData(request: ProcessDataRequest): Promise<ProcessDataResponse>;
+	onNewEvent(callback: newEventCallbackFunction): void;
+}
