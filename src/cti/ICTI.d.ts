5edca069ae60798356a9103487f1762441704b52
vertikapandey <122857590+vertikapandey@users.noreply.github.com>
2024-03-14 10:42:51 +0530

    Interfaces and implementations (#5)
    * Adding CTIDriver interface and implementation
    
    * Added copilot adpater interface and implementation
    
    * Added copywright headers in src files
    
    * remove copilotAdapter code and updated the ctiDriver interface descriptions
    
    * Removed exampleCTIDRiver file
    
    * Update ICTI.d.ts
    
    Signed-off-by: vertikapandey <122857590+vertikapandey@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: vertikapandey <122857590+vertikapandey@users.noreply.github.com>
    Co-authored-by: Vertika Pandey <vepandey@microsoft.com>

---
+++ src/cti/ICTI.d.ts
@@ -0,0 +1,110 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export interface CustomerInfo {
+	phonenumber?: string;
+	email?: string;
+	name?: string;
+}
+
+interface ClickDialPayloadInfo {
+	number: string;
+	objectType: string;
+	recordId: string;
+	recordName: string;
+}
+
+export interface ConversationInfo {
+	conversationId: string;
+	customerData: CustomerInfo;
+}
+
+interface RecordDetails {
+	name?: string;
+	firstName?: string;
+	lastName?: string;
+	email?: string;
+	phonenumber?: string;
+}
+
+interface ConversationTaskDetails {
+    subject: string;
+    status: string;
+    record: RecordDetails;
+}
+
+export interface ConversationDetails{
+    [ConversationId: string]: ConversationTaskDetails;
+}
+
+type ClickToDialCallbackFunction = (payload: ClickDialPayloadInfo) => void;
+
+export interface ICTIInterface {
+
+      /**
+       * @description Initialize the scripts and do the operations once it is loaded
+       * @returns Promise void
+       */
+     initialize():Promise<boolean>;
+
+      /**
+       * @description Function to be called when conversation is ready. Could be used for operations like 
+       *              opening up contact/account record in third party crm system
+       * @param {ConversationInfo} conversationData Object containing conversation details and customer data.
+       * @returns void 
+       * @example conversationReady({ conversationId: "c6e27f69-c9ea-4547-8040-dc26c2712c07" , 
+       *                              customerData: { phonenumber: "+1-212-456-7890" , email: "firstname.secondname@domain.com" , name: "John"}})
+       */
+    conversationReady(conversationData: ConversationInfo): Promise<void>;
+
+     /**
+      * @description Function for ending a conversation.
+      *              Add operations to be performed when conversation is ended,
+      *              like saving the conversation transcript in the contact
+      * @param {string} conversationId - Unique identifier of the conversation that is being ended.
+      * @returns void
+      * @example endConversation("b46d915e-f7da-4c58-80e9-9f842517617b")
+      */  
+    endConversation(conversationId: string): void;
+    
+    /**
+     * @description Function to handle operations on Click to dial, Add operations to be performed when outbound call is performed 
+     * @param {ClickToDialCallbackFunction} callbackFuntion function to be registered for click-to-dial.
+     * @returns void
+     * @example onClickToDial((payload: ClickToDialCallbackFunction) => {// implementation})
+     */
+    onClickToDial(callbackFuntion: ClickToDialCallbackFunction): void;
+    
+    /**
+     * @description Funtion to set visibility of softphone panel
+     * @param {boolean} visible flag to indicate visibility
+     * @returns void
+     * @example setSoftPhonePanelVisibility(true)
+     */
+    setSoftPhonePanelVisibility(visible: boolean): void;
+    
+    /**
+     * @description Funtion to set width of softphone panel
+     * @param {number} width number to set width
+     * @returns void
+     * @example setSoftPhonePanelWidth(500)
+     */
+    setSoftPhonePanelWidth(width: number): void;
+    
+    /**
+     * @description Funtion to set height of softphone panel
+     * @param {number} height number to set height
+     * @returns void
+     * @example setSoftPhonePanelHeight(748)
+     */
+    setSoftPhonePanelHeight(height: number): void;
+    
+    /**
+     * @description Function to get conversation details
+     *              Query conversations inside third party crm system with the conversation ids in the parameter, return the result of the query
+     * @param {string[]} conversationIds Array of ids
+     * @returns {Promise<ConversationDetails[]>} Array of conversation details
+     * @example getConversationDetails(["fab5422f-32a2-49c7-ad19-b3d713c558d0" , "b46d915e-f7da-4c58-80e9-9f842517617b"])
+     */
+    getConversationDetails(conversationIds: string[]): Promise<ConversationDetails[]>;
+}
